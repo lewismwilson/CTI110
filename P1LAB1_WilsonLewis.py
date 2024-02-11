7348ceabeb2bbcc87ce29ba09af950233a927356
lewismwilson <159702836+lewismwilson@users.noreply.github.com>
2024-02-11 08:47:16 -0500

    Add files via upload
    p1lab1 first submission

---
+++ P1LAB1_WilsonLewis.py
@@ -0,0 +1,8 @@
+# Lewis Wilson
+# 11 February 2024
+# P 1 Lab 1
+# A simple program that outputs a welcome message to a user...
+
+your_name = input('What is your name?')
+print('Hey', your_name)
+print('Welcome to CTI 110')
