c7e115c133efdc77fcb685a3414657c0665c5e7c
lewismwilson <159702836+lewismwilson@users.noreply.github.com>
2024-02-11 09:03:20 -0500

    Add files via upload
    p1HW1 first submission

---
+++ P1HW1_WilsonLewis.py
@@ -0,0 +1,14 @@
+# Lewis Wilson
+# 11 February 2024
+# P1HW1
+# a program that takes given number and outputs math solutions
+
+your_int = int(input('Enter integer:'))
+your_int_squared = your_int * your_int
+your_int_cubed = your_int * your_int * your_int
+print('You entered:', your_int)
+print(your_int, 'squared is', your_int_squared)
+print('And', your_int, 'cubed is', your_int_cubed)
+another_int = int(input('Enter  another integer:'))
+print(your_int, '+', another_int, 'is', your_int + another_int)
+print(your_int, '*', another_int, 'is', your_int * another_int)
